7bab2cd90a2ae9b8ef43ec776cf8c79aed8bf290
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-17 23:33:43 +0000

    triggers if cond

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,12 +5,8 @@ BEFORE UPDATE
 ON users
 FOR EACH ROW
 BEGIN
-	IF OLD.email != NEW.email THEN
-		IF NEW.valid_email = 0 THEN
-			SET NEW.valid_email = 1;
-		ELSE
-			SET NEW.valid_email = 0;
-		END IF;
+	IF OLD.email <> NEW.email THEN
+		SET NEW.valid_email = 0;
 	END IF;
 END;
 |
