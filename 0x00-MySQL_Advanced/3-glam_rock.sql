ea9e5aea69e008ddf33cecb0a1552d9ef9250c86
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-17 20:58:19 +0000

    task3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,2 @@
+-- script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT `band_name`, COALESCE(split - formed, 2022 - formed) AS `lifespan` FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style) > 0;
