499109fcfe000dd8b16fea84a5fbf445101ca6c6
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-18 12:51:27 +0000

    task 7 procedures avg score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,7 +2,7 @@
 DELIMITER |
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	SET @AV = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
+	SET @AV = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
 	UPDATE users SET average_score = @AV WHERE id = user_id;
 END;
 |
