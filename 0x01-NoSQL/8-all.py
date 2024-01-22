dab6f24f2495847b3220a385fce25c5482c86e42
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-22 19:46:13 +0000

    list all documents

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Write a Python function that lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """
+        list all documents
+        Args:
+            mongo_collection: passed collection
+        Return - found documents in the passed collection
+    """
+    documents = list(mongo_collection.find())
+    if len(documents) > 0:
+        return documents
+    return []
