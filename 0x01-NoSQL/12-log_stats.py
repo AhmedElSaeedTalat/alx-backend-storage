85fa645c2a00784c666fe564afa8dbfa5373f747
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-23 00:38:52 +0000

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,21 +8,21 @@ def display_nginx():
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
     count_documents = collection.count_documents({})
-    print(count_documents, 'logs')
+    print(f'{count_documents} logs')
     print('Methods:')
     get = list(collection.find({'method': 'GET'}))
     post = list(collection.find({'method': 'POST'}))
     put = list(collection.find({'method': 'PUT'}))
     pach = list(collection.find({'method': 'PATCH'}))
     delete = list(collection.find({'method': 'DELETE'}))
-    print('\tmethod GET:', len(get))
-    print('\tmethod POST', len(post))
-    print('\tmethod PUT:', len(put))
-    print('\tmethod PATCH:', len(pach))
-    print('\tmethod DELETE:', len(delete))
+    print(f'\tmethod GET: {len(get)}')
+    print(f'\tmethod POST {len(post)}')
+    print(f'\tmethod PUT: {len(put)}')
+    print(f'\tmethod PATCH: {len(pach)}')
+    print(f'\tmethod DELETE: {len(delete)}')
 
     get_status = list(collection.find({'method': 'GET', 'path': '/status'}))
-    print(len(get_status), 'status check')
+    print(f'{len(get_status)} status check')
 
 
 if __name__ == "__main__":
