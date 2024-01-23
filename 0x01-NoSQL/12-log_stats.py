c418381f27c7c9bf64dfbfbf38c0c1be3c474521
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-23 00:24:23 +0000

    task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" task 12 """
+from pymongo import MongoClient
+
+
+def display_nginx():
+    """ display data of nginx """
+    client = MongoClient('localhost', 27017)
+    collection = client.logs.nginx
+    count_documents = collection.count_documents({})
+    print(count_documents, 'logs')
+    print('Methods:')
+    get = list(collection.find({'method': 'GET'}))
+    post = list(collection.find({'method': 'POST'}))
+    put = list(collection.find({'method': 'PUT'}))
+    pach = list(collection.find({'method': 'PATCH'}))
+    delete = list(collection.find({'method': 'DELETE'}))
+    print('\tmethod GET:', len(get))
+    print('\tmethod POST', len(post))
+    print('\tmethod PUT:', len(put))
+    print('\tmethod PATCH:', len(pach))
+    print('\tmethod DELETE:', len(delete))
+
+    get_status = list(collection.find({'method': 'GET', 'path': '/status'}))
+    print(len(get_status), 'status check')
+
+
+display_nginx()
