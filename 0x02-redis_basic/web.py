80b4b72512b10027a9c2f79af9a47c5083c5f625
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-24 20:56:43 +0000

    Task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -15,7 +15,7 @@ def cache_countVisited(method: Callable) -> Callable:
         key = f'count:{url}'
         db.incr(key)
         if db.get(f'result:{url}'):
-            return db.get(f'result:{url}')
+            return db.get(f'result:{url}').decode('utf-8')
         result = method(url)
         db.setex(f'result:{url}', 10, result)
         return result
