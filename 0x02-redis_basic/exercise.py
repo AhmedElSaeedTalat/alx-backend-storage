0e78b30aecbab84e91fc8b6b9ff8453ef4151fd8
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-24 11:34:44 +0000

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ store data file
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -24,3 +24,27 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str,
+                                                                    bytes,
+                                                                    int,
+                                                                    float]:
+        """
+            get stored value
+            Args:
+                key: key for value stored to retrieve
+                fn: callable of a function
+            Return - data stored
+        """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ parametrize Cache.get with string conversion """
+        return self.get(key, lambda val: val.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """ parametrize Cache.get with int conversion """
+        return self.get(key, lambda val: int(val.decode('utf-8')))
